5cefacdb4cf85bdc03a396e35e1c3ae69464d4af
Gru3y <Gru3y>
2023-08-13 10:36:52 +0200

    Second commit - parser create

--- scanner.py
+++ scanner.py
@@ -1,1 +1,8 @@
 import argparse
+
+parser = argparse.ArgumentParser(description="Simple TCP/UDP Scanner")
+parser.add_argument('adres', type=str, help='IP address')
+parser.add_argument('-sT', action='store_true', help='TCP Scan')
+parser.add_argument('-sU', action='store_true', help='UDP Scan')
+
+args = parser.parse_args()
