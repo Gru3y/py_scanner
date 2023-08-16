876d180063dcf9cd1e3be23cbb4fca51f34239e5
Gru3y <Gru3y>
2023-08-16 20:30:44 +0200

    add perform_ping func

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 import argparse
 import socket
 import pyfiglet
+from ping3 import ping
 
 parser = argparse.ArgumentParser(description="Simple TCP/UDP Port Scanner")
 parser.add_argument('address', type=str, help='IP address')
@@ -9,7 +10,6 @@ parser.add_argument('-sU', action='store_true', help='UDP Scan')
 parser.add_argument('-p', type=int, help='Port to scan')
 
 args = parser.parse_args()
-
 
 def main():
     header = pyfiglet.figlet_format('PyScanner')
@@ -33,19 +33,25 @@ def udp_scan():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(1)
     try:
+        reach_addr = perform_ping(args.address)
         sock.sendto(b'Payload', (args.address, args.p))
-        data, addr = sock.recvfrom(1024)
+        sock.recvfrom(1024)
         print('Port is open.')
-    except TimeoutError:
-        print("The server didn't respond so the scanned port may be open.")
-    except ConnectionResetError:
-        print('Port is closed.')
+    except TimeoutError or ConnectionResetError:
+        print(f"The server didn't respond but the scanned port may be open. {reach_addr}")
     except OverflowError:
         print('Port must be 0-65535')
     except socket.gaierror:
         print('Invalid hostname or IPv4 address.')
     finally:
         sock.close()
+
+def perform_ping(ip_addr):
+    reply = ping(ip_addr)
+    if reply is not None:
+        return f"{ip_addr} is reachable."
+    else:
+        return f"{ip_addr} is not reachable."
 
 
 '''if __name__ == '__main__':
