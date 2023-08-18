e2678a3ef6b824ef3a0adaf341bca474f893358e
Gru3y <Gru3y>
2023-08-18 20:49:21 +0200

    Refactoring and use main function

--- scanner.py
+++ scanner.py
@@ -10,26 +10,14 @@ def parse_port_range(port_range):
         port_range_list = list(range(start, end + 1))
     return port_range_list
 
-parser = argparse.ArgumentParser(description="Simple TCP/UDP Port Scanner")
-parser.add_argument('address', type=str, help='IP address')
-parser.add_argument('-sT', action='store_true', help='TCP Scan')
-parser.add_argument('-sU', action='store_true', help='UDP Scan')
-#parser.add_argument('--top-ports' action='store_true', help='Top ports scan')
-parser.add_argument('-p', type=int, help='Port to scan')
-parser.add_argument('-r', '--range', type=parse_port_range, help='Range of ports to scan (e.g. 20-100)')
+def perform_ping(ip_addr):
+    reply = ping(ip_addr)
+    if reply is not None:
+        return f"{ip_addr} is reachable."
+    else:
+        return f"{ip_addr} is not reachable."
 
-args = parser.parse_args()
-
-def main():
-    header = pyfiglet.figlet_format('PyScanner')
-    print(header)
-    '''if args.sT:
-        tcp_scan()
-    
-    elif args.sU:
-        udp_scan()'''
-
-def tcp_scan():
+def tcp_scan(args):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         r = sock.connect_ex((args.address, args.p))
@@ -42,8 +30,9 @@ def tcp_scan():
         print('Port must be 0-65535')
     except socket.gaierror:
         print('Invalid hostname or IPv4 address.')
+    
 
-def udp_scan():
+def udp_scan(args):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(1)
     try:
@@ -60,8 +49,8 @@ def udp_scan():
     finally:
         sock.close()
 
-def tcp_top_ports():
-    top_ports = [21, 22, 23, 25, 53, 80, 110, 111, 139, 143, 443, 445, 993, 995, 1433, 1723, 3306, 3389, 5432, 5900, 8080]
+def tcp_top_ports_scan(args):
+    top_ports = [21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1433, 1723, 3306, 3389, 5432, 5900, 8080]
     open_ports = []
     
     for port in top_ports:
@@ -77,7 +66,7 @@ def tcp_top_ports():
 
     print(open_ports)
 
-def scan_port_range():
+def scan_port_range(args):
     open_ports = []
     for port in args.range:
         try:
@@ -91,13 +80,36 @@ def scan_port_range():
             pass
     print(open_ports)
 
-def perform_ping(ip_addr):
-    reply = ping(ip_addr)
-    if reply is not None:
-        return f"{ip_addr} is reachable."
-    else:
-        return f"{ip_addr} is not reachable."
 
 
-'''if __name__ == '__main__':
-    main()'''
+def main():
+    header = pyfiglet.figlet_format('PyScanner')
+    print(header)
+
+    parser = argparse.ArgumentParser(description="Simple TCP/UDP Port Scanner")
+    parser.add_argument('address', type=str, help='IP address') 
+    parser.add_argument('-sT', action='store_true', help='TCP Scan') 
+    parser.add_argument('-sU', action='store_true', help='UDP Scan') 
+    parser.add_argument('--top-ports', '-tp', action='store_true', help='Top ports scan')
+    parser.add_argument('-p', type=int, help='Port to scan') 
+    parser.add_argument('--range', '-r', type=parse_port_range, help='Range of ports to scan (e.g. 20-100)') 
+
+    args, unknown_args = parser.parse_known_args()
+
+    try:
+        if unknown_args:
+            print(f"Unrecognized parameters: {unknown_args}")
+        elif args.sT and args.range:
+            scan_port_range(args)
+        elif args.sT:
+            tcp_scan(args)
+        elif args.top_ports:
+            tcp_top_ports_scan(args)
+        elif args.sU:
+            udp_scan(args)
+    except argparse.ArgumentError:
+        print('Fatal Parse Error')
+
+
+if __name__ == '__main__':
+    main()
